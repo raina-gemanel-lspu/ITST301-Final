dc8d3fd15a7421b5d6f368cd1bd694dfac72768a
Raina Gemanel <raina.gemanel@lspu.edu.ph>
2022-12-09 18:16:16 +0800

    123

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$to = "raina.gemanel@lspu.edu.ph"
+
+$headers = "From: ".$name. "\r\n" .
+"CC: "
+
+?>
